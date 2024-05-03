7d9e5a8aba9b69f0fa9178c880608398844b2e25
Sanjuwave01 <154417542+Sanjuwave01@users.noreply.github.com>
2024-05-03 15:28:31 +0530

    Update trc20usdt.js

--- routes/trc20usdt.js
+++ routes/trc20usdt.js
@@ -24,68 +24,64 @@ router.get("/", async (req, res) => {
 });
 
 router.post('/transfer', verifyuser, async (req, res) => {
-    const HttpProvider = TronWeb.providers.HttpProvider;
     try {
-        const fullNode = new HttpProvider("https://api.trongrid.io");
 
-        const solidityNode = new HttpProvider("https://api.trongrid.io");
-        const eventServer = new HttpProvider("https://api.trongrid.io");
-        const privateKey = req.body.privateKey;
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
-        const account = tronWeb.address.fromPrivateKey(req.body.privateKey);
-
-        const CONTRACT = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";
-
-        const ACCOUNT = account;
-        if (!tronWeb.isAddress(req.body.to)) {
-            return res.status(403).json({
-                message: "Address not found",
-
-            })
-        }
-        const {
-            abi
-        } = await tronWeb.trx.getContract(CONTRACT);
-        // console.log(JSON.stringify(abi));
-
-        const contract = tronWeb.contract(abi.entrys, CONTRACT);
-
-        const balance = await contract.methods.balanceOf(ACCOUNT).call();
-        const exactbalance = balance.toString() / 1000000;
-        console.log("balance:", exactbalance);
-        if (exactbalance) {
-            await contract.transferFrom(
-                ACCOUNT, //address _from
-                req.body.to, //address _to
-                req.body.amount-0.5 //amount
-            ).send({
-                feeLimit: 100000
-            }).then(output => {
-
-                console.log('- Output:', output, '\n');
-
-                return res.status(200).json({
-                    output
-
-                })
-
-            })
+      const HttpProvider = TronWeb.providers.HttpProvider;
+      const fullNode = new HttpProvider("https://api.trongrid.io");
+      // const fullNode = new HttpProvider("http://192.168.1.162:8090");
+      const solidityNode = new HttpProvider("https://api.trongrid.io");
+      const eventServer = new HttpProvider("https://api.trongrid.io");
+      const privateKey = req.body.privateKey;
+      const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
 
 
+      const CONTRACT = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";
 
+      const ACCOUNT = req.body.from;
+      if(!tronWeb.isAddress(req.body.to)){
+          return res.status(403).json({
+              message: "Address not found",
 
-        } else {
-            return res.status(403).json({
-                message: "Insufficent Balance",
+          })
+      } 
+      const {
+          abi
+      } = await tronWeb.trx.getContract(CONTRACT);
+      // console.log(JSON.stringify(abi));
+
+      const contract = tronWeb.contract(abi.entrys, CONTRACT);
+
+      const balance = await contract.methods.balanceOf(ACCOUNT).call();
+      const exactbalance = balance.toString() / 1000000;
+      console.log("balance:", exactbalance);
+      if (exactbalance) {
+        await contract.transferFrom(
+            ACCOUNT, //address _from
+            req.body.to, //address _to
+            req.body.amount-0.5 //amount
+        ).send({
+            feeLimit: 100000
+        }).then(output => {
+
+            console.log('- Output:', output, '\n');
+
+            return res.status(200).json({
+                output
 
             })
-        }
-    } catch (error) {
-        return res.status(403).json({
-            error
 
-        })
-    }
+        });
+      } else {
+         return res.status(403).json({
+              message: "Insufficent Balance",
+
+          })
+      }
+
+
+  } catch (error) {
+      console.log(error)
+  }
 })
 
 
